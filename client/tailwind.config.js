486bc6d11de905c65810bcfaa7d4f3eafcc7424f
vmphung <phung.vo@kyanon.digital>
2024-01-28 21:10:23 +0700

    [Install] Installing modules/set up mini ecomerce project

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
