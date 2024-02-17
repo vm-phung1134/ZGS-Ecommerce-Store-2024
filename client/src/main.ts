1cae1baef8784eca75b607d83d525a774b7be8a9
vmphung <phung.vo@kyanon.digital>
2024-02-17 16:26:48 +0700

    [Installing] set up source with vuex

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./routes";
+import store from "./store";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(store).mount("#app");
