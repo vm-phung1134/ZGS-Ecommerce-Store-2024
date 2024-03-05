d7af9f1ab4eacad387fd90d937053a84044d8636
vmphung <phung.vo@kyanon.digital>
2024-03-05 18:15:26 +0700

    [Add] Add product detail page

--- client/src/pages/ProductDetail.vue
+++ client/src/pages/ProductDetail.vue
@@ -1,129 +1,161 @@
 <template>
-    <div class="h-[120vh]">
-        <!-- component -->
-        <section class="text-gray-700 body-font overflow-hidden bg-white">
-            <div class="container px-5 py-24 mx-auto">
-                <div class="lg:w-4/5 mx-auto flex flex-wrap">
-                    <img alt="ecommerce" class="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200"
-                        src="https://www.whitmorerarebooks.com/pictures/medium/2465.jpg">
-                    <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                        <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
-                        <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
-                        <div class="flex mb-4">
-                            <span class="flex items-center">
-                                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500"
-                                    viewBox="0 0 24 24">
-                                    <path
-                                        d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
-                                    </path>
-                                </svg>
-                                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500"
-                                    viewBox="0 0 24 24">
-                                    <path
-                                        d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
-                                    </path>
-                                </svg>
-                                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500"
-                                    viewBox="0 0 24 24">
-                                    <path
-                                        d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
-                                    </path>
-                                </svg>
-                                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
-                                    stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500"
-                                    viewBox="0 0 24 24">
-                                    <path
-                                        d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
-                                    </path>
-                                </svg>
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
-                                    <path
-                                        d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
-                                    </path>
-                                </svg>
-                                <span class="text-gray-600 ml-3">4 Reviews</span>
-                            </span>
-                            <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
-                                <a class="text-gray-500">
-                                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        class="w-5 h-5" viewBox="0 0 24 24">
-                                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                                    </svg>
-                                </a>
-                                <a class="ml-2 text-gray-500">
-                                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        class="w-5 h-5" viewBox="0 0 24 24">
-                                        <path
-                                            d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
-                                        </path>
-                                    </svg>
-                                </a>
-                                <a class="ml-2 text-gray-500">
-                                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        class="w-5 h-5" viewBox="0 0 24 24">
-                                        <path
-                                            d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
-                                        </path>
-                                    </svg>
-                                </a>
-                            </span>
-                        </div>
-                        <p class="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha
-                            taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage
-                            brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle
-                            pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
-                        <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
-                            <div class="flex">
-                                <span class="mr-3">Color</span>
-                                <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                                <button
-                                    class="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                                <button
-                                    class="border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none"></button>
+    <div>
+        <div
+            class="h-[120vh] bg-cover bg-[url('https://storage-asset.msi.com/global/picture/image/feature/monitor/MEG-342C-QD-OLED/kv-bg.jpg')] w-full">
+            <!-- component -->
+            <section class="text-gray-700 body-font overflow-hidden">
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="flex gap-10 mx-20">
+                        <img alt="ecommerce" class="w-1/2 object-cover object-center rounded"
+                            src="https://storage-asset.msi.com/global/picture/image/feature/monitor/MEG-342C-QD-OLED/story-pd.png">
+                        <div class="w-1/2 flex flex-col gap-5 p-8 bg-black/80 text-white">
+                            <div>
+                                <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
+                                <h1 class="text-3xl title-font font-bold">GeForce RTX™ 4090 VENTUS 3X 24G</h1>
                             </div>
-                            <div class="flex ml-6 items-center">
-                                <span class="mr-3">Size</span>
-                                <div class="relative">
-                                    <select
-                                        class="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
-                                        <option>SM</option>
-                                        <option>M</option>
-                                        <option>L</option>
-                                        <option>XL</option>
-                                </select>
-                                <span
-                                    class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round"
-                                        stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
-                                        <path d="M6 9l6 6 6-6"></path>
+                            <div class="flex mb-4">
+                                <span class="flex items-center">
+                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-orange-500"
+                                        viewBox="0 0 24 24">
+                                        <path
+                                            d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
+                                        </path>
                                     </svg>
+                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-orange-500"
+                                        viewBox="0 0 24 24">
+                                        <path
+                                            d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
+                                        </path>
+                                    </svg>
+                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-orange-500"
+                                        viewBox="0 0 24 24">
+                                        <path
+                                            d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
+                                        </path>
+                                    </svg>
+                                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
+                                        stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-orange-500"
+                                        viewBox="0 0 24 24">
+                                        <path
+                                            d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
+                                        </path>
+                                    </svg>
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" class="w-4 h-4 text-orange-500" viewBox="0 0 24 24">
+                                        <path
+                                            d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z">
+                                        </path>
+                                    </svg>
+                                    <span class="text-orange-500 ml-3">4 Reviews</span>
+                                </span>
+                                <span class="flex ml-3 pl-3 border-l-2 border-gray-100">
+                                    <a class="text-blue-500">
+                                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                        </svg>
+                                    </a>
+                                    <a class="ml-2 text-blue-500">
+                                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                            <path
+                                                d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
+                                            </path>
+                                        </svg>
+                                    </a>
+                                    <a class="ml-2 text-blue-500">
+                                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                            <path
+                                                d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z">
+                                            </path>
+                                        </svg>
+                                    </a>
                                 </span>
                             </div>
+                            <p class="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric
+                                sriracha
+                                taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage
+                                brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle
+                                pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.
+                            </p>
+                            <img class="w-1/2"
+                                src="https://storage-asset.msi.com/global/picture/image/feature/monitor/MEG-342C-QD-OLED/kv-icons.png"
+                                alt="image-detail-product">
+                            <div class="flex h-fit mt-auto items-center justify-between">
+                                <span class="title-font font-bold text-2xl">$146.00</span>
+                                <button class="bg-red-600 uppercase text-sm font-bold text-white py-3 px-10">Place an
+                                    order</button>
+                            </div>
                         </div>
                     </div>
-                    <div class="flex">
-                        <span class="title-font font-medium text-2xl text-gray-900">$58.00</span>
-                        <button
-                            class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Button</button>
-                        <button
-                            class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                class="w-5 h-5" viewBox="0 0 24 24">
-                                <path
-                                    d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z">
-                                </path>
-                            </svg>
-                        </button>
-                    </div>
                 </div>
+            </section>
+        </div>
+        <div class="grid grid-cols-3 gap-5 m-20">
+            <!-- Item 1 -->
+            <div class="border py-10 flex flex-col gap-5 justify-center items-center">
+                <div>
+                    <img class="w-14"
+                        src="https://storage-asset.msi.com/global/picture/image/feature/nb/Thin/Thin15-B13V/images/ft-icon-windows11.png"
+                        alt="">
+                </div>
+                <h3 class="text-xl text-blue-800">Windows 11 Home</h3>
+            </div>
+            <!-- Item 2 -->
+            <div class="border py-10 flex flex-col gap-5 justify-center items-center">
+                <div>
+                    <img class="w-14"
+                        src="https://storage-asset.msi.com/global/picture/image/feature/nb/2023_RPLS/Cyborg-15-AI-A1V/icon-core-ultra-processor-7.webp"
+                        alt="">
+                </div>
+                <h3 class="text-xl text-blue-800">Intel® Core™ Ultra 7 processor 155H</h3>
+            </div>
+            <!-- Item 3 -->
+            <div class="border py-10 flex flex-col gap-5 justify-center items-center">
+                <div>
+                    <img class="w-full"
+                        src="https://storage-asset.msi.com/global/picture/image/feature/Handhelds/Claw-A1M/icon-Ergonomic-Design-v1.png"
+                        alt="">
+                </div>
+                <h3 class="text-xl text-blue-800">Long-session Comfort Gaming</h3>
+            </div>
+            <!-- Item 4 -->
+            <div class="border py-10 flex flex-col gap-5 justify-center items-center">
+                <div>
+                    <img class="w-full"
+                        src="https://storage-asset.msi.com/global/picture/image/feature/Handhelds/Claw-A1M/icon-Cooler-Boost-HyperFlow-v1.png"
+                        alt="">
+                </div>
+                <h3 class="text-xl text-blue-800">Keep Cooler, Game Longer</h3>
+            </div>
+            <!-- Item 5 -->
+            <div class="border py-10 flex flex-col gap-5 justify-center items-center">
+                <div>
+                    <img class="w-14"
+                        src="https://storage-asset.msi.com/global/picture/image/feature/Handhelds/Claw-A1M/icon-53-WHr-Battery-Capacity-v1.png"
+                        alt="">
+                </div>
+                <h3 class="text-xl text-blue-800">Design dedicated for Handheld</h3>
+            </div>
+            <!-- Item 6 -->
+            <div class="border py-10 flex flex-col gap-5 justify-center items-center">
+                <div>
+                    <img class="w-14"
+                        src="https://storage-asset.msi.com/global/picture/image/feature/Handhelds/Claw-A1M/MSICenterM-icon.png"
+                        alt="">
+                </div>
+                <h3 class="text-xl text-blue-800">Long-session Comfort Gaming</h3>
             </div>
         </div>
-    </section>
-</div></template>
+        <div>
+            <img src="https://storage-asset.msi.com/global/picture/image/feature/Handhelds/Claw-A1M/wifi7-v1.webp" alt="">
+        </div>
+    </div>
+</template>
 
 <script setup lang="ts">
 
